a7db6e079f8f0c4cc8711656a818edfe30a493a9
logan-codes <loganandamuthu2005@gmail.com>
2025-09-02 23:50:51 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Component } from "@/components/comp-578"
+import "@/styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
